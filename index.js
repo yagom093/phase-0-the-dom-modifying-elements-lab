2d82cf2537c0d6561bb7b81f9da33501451bf4da
yago <yagom10@hotmail.com>
2023-04-21 12:17:52 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.querySelector('main')
+main.remove()
+let newHeader=document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML="Yago is the champion"
